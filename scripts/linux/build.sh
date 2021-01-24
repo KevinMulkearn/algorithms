cfe1ce57a437759b4bea8558c92b3d8bf6e30cc5
Kevin <kevin.mul@hotmail.com>
2021-01-24 22:43:50 +0000

    Build linux use all cores

--- scripts/linux/build.sh
+++ scripts/linux/build.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/bash
 
 PROJECT_DIR=$(cd "$(dirname $0)/../.." && pwd)
+NUM_CPUS=$(nproc)
 
 cmake  -DCMAKE_BUILD_TYPE=Debug -S "${PROJECT_DIR}" -B "${PROJECT_DIR}/build"
 
 cd "${PROJECT_DIR}/build"
 
-make
+make -j"${NUM_CPUS}"
